9a0916f2a5de649c677e26a9fa79c38d3f67323b
miles zarathustra <mlzarathustra@gmail.com>
2015-11-02 07:30:19 +0100

    added only-candidate inference

--- v2/s/Game.groovy
+++ v2/s/Game.groovy
@@ -5,9 +5,7 @@ class Game {
     static boolean D=false // debug
     static boolean D2=false // show function entries
     static boolean D3=false
-    boolean cloneInferences=false
 
-    def stack=[] as List<Board>
     Game(String inp) { stack << new Board( inp ) }
 
     void showStack() { println stack.join('\n'+'  .'*30+'\n') }
@@ -18,41 +16,6 @@ class Game {
                 }
                 .join('\n'+'  .'*30+'\n')
         )
-    }
-
-    boolean win() { stack.last().win() }
-    boolean lose() { stack.last().lose() }
-    boolean full() { stack.last().full() }
-
-    Board lastBoard() {
-        Board b = (cloneInferences) ? stack.last().clone() : stack.last()
-        b.findPossible()
-        if (b.full() && !b.win()) println "lastBoard(): erroneous state"
-        b
-    }
-
-    // if there is only one candidate Tile for a digit
-    // within any coterie, set it to that digit
-    boolean inferByOnlyCandidate() {
-        if (D2) println "inferByOnlyCandidate"
-        Board b=lastBoard()                                     // OK HERE
-
-        boolean hasInferredSingletons = b.inferSingletons()
-        if (b.lose()) {
-            println "ran out of possibilities!!"
-            return hasInferredSingletons
-        }
-        if (hasInferredSingletons) {
-            if (cloneInferences) stack.add(b);
-            if (D) println b
-        }
-                                                               // BREAKS HERE
-        if (b.full() && !b.win()) {
-            println "inferByOnlyCandidate: erroneous state"
-            return false
-        }
-
-        hasInferredSingletons
     }
 
     // if there is only one possible digit for a Tile,
@@ -68,16 +31,7 @@ class Game {
             if (D) println b
         }
     }
-/*
-    void infer() {
-        boolean doom=true  // groovy doesn't offer do/while
-        while (doom) {
-            def hasInferredSingletons = inferByOnlyCandidate()
-            def hasSingletons = inferByOnlyPossible()
-            if (!(hasInferredSingletons || hasSingletons) ) doom=false
-        }
-    }
-*/
+
     // return: winning board, or null
     //
     static Board guess(Board b) {
@@ -85,9 +39,15 @@ class Game {
         b=b.clone()
         if (D3) println "GUESS!"
 
+        //  inferences
         inferByOnlyPossible(b)
         if (b.win()) return b
         if (b.lose() || b.improper()) return null
+
+        while (b.inferSingletons())
+        if (b.win()) return b
+        if (b.lose() || b.improper()) return null
+        //
 
         Tile t=b.findSmallestVariant()
         if (D3) println "smallest variant is $t"
@@ -106,19 +66,6 @@ class Game {
         }
         return null
     }
-/*
-    static Board guess(String inp) {
-        Board win=guess(new Board(inp))
-        if (win==null) println "no solution found."
-        else println "WIN!! \n\n$win"
-        return win
-    }
-*/
-/*
-    static Board solve(Board b) {
-        guess(b)
-    }
-*/
 
     static def solve(String inp) {
         Board puzzle = new Board(inp)
@@ -138,6 +85,5 @@ class Game {
 
         println '- '*45
     }
-    
-    
+
 }
